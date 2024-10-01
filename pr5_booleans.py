ae16b9c88e8fa6dc3b0da3096db2675296cfd632
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-09-30 22:46:50 -0600

    Add files via upload

---
+++ pr5_booleans.py
@@ -0,0 +1,16 @@
+ 
+print (" ") #Espacio a agregar
+print ("Lira Hernandez Dayana Yamileth: Practica que indica si un numero es mayor, menor o igual que otro")
+print (" ") #Espacio a agregar
+
+a = 50           #Valor de variable ´a´
+b = 100          #Valor de variable ´b´
+
+if b > a:           #if inicia el proceso de las sig. funciones
+  print("b es mayor que a")   #Imprime la funcion a presentar
+else:                        #else inicia la ultima funcion
+  print("b no es mayor que a") #Imprime la funcion a presentar
+
+print (" ") #Espacio a agregar
+
+
